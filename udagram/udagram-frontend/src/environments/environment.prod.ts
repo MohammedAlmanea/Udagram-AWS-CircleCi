f2d945757fea24adf2103003fe6193a146d95757
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-01-07 08:06:32 +0300

    Lint fixes

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-wgsa4svp.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost:
+    'http://udagram-api-dev.eba-wgsa4svp.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
